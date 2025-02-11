cbfbf7e95c95c0f8d8885f961814f1b009cc578f
Frans-Spiesschaert <58788694+Frans-Spiesschaert@users.noreply.github.com>
2025-02-11 20:49:45 +0100

    Make website operational.

---
+++ statcounter/statcounter_vars.js
@@ -0,0 +1,7 @@
+<!-- Tele2 StatCounter Code vars -->
+var sc_project=4444938;
+var sc_invisible=1;
+var sc_partition=55;
+var sc_click_stat=1;
+var sc_security="47bc06d4";
+<!-- End of Tele2 StatCounter Code vars -->
